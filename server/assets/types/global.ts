ea73f50295c1bf2154dbe2c0a228d4c1ad0a28d1
Shahul Hammeed <53307156+shahul01@users.noreply.github.com>
2022-08-26 02:29:02 +0530

    feat: added basic socket event; fix/chore: added ts types

---
+++ server/assets/types/global.ts
@@ -0,0 +1,18 @@
+// ### Socket  ###
+export enum ESocketEventsDict {
+  "connection" = "connection",
+  "joinRoom" = "joinRoom",
+  "ping" = "ping",
+  "pong" = "pong",
+  "typing" = "typing",
+  "stopTyping" = "stopTyping",
+  "serverMessage" = "serverMessage",
+  "clientMessage" = "clientMessage",
+  "disconnect" = "disconnect",
+  "reconnect" = "reconnect"
+}
+
+export interface IServerMessageData {
+  id: number;
+  message: string;
+}
--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,12 @@
+/*
+ *  Socket.IO Server
+ *
+ *  Basic:
+ *    Client message → Server → Server message → All clients
+*/
+
+
+// COMMT: Imports
 import cors from 'cors';
 import { Server } from 'socket.io';
 import express from 'express';
@@ -5,7 +14,9 @@ import http from 'http';
 import indexRouter from './routes/indexRoutes';
 // import { socketEventsDict } from './helpers/socketEvents';
 import { consoleLine } from './helpers/misc';
+import { ESocketEventsDict, IServerMessageData } from './assets/types/global';
 
+// COMMT: Setup
 const app = express();
 const server = http.createServer(app);
 const io  = new Server(server, {
@@ -20,15 +31,34 @@ const port = process.env?.PORT || 8000;
 app.use(cors());
 app.use(indexRouter);
 
-
+// COMMT: Socket IO - Main code
 io.on(ESocketEventsDict['connection'], (socket) => {
-  console.log(`socket.id: `, socket.id);
-  // socket.on(socketEventsDict[''], () => {});
+  console.log(`Connected: `, socket.id);
+
+
+  // COMMT: Emit back serverMessage
+  socket.on(
+    ESocketEventsDict['clientMessage'],
+    ( data: IServerMessageData, callback: () => void ) => {
+
+      const serverMessageData = {
+        id: data?.id,
+        message: data?.message
+      };
+
+      socket.emit(
+        ESocketEventsDict['serverMessage'],
+        serverMessageData
+      );
+
+      callback();
+
+  });
 
 });
 
 
-
+// COMMT: Server listens to port
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 
