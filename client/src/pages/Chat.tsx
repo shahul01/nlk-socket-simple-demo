b9fc02c3c47e81c6aa11b30ec3f40b536158ba7f
Shahul Hammeed <53307156+shahul01@users.noreply.github.com>
2022-08-26 05:22:26 +0530

    feat: added bare starter components and folders

---
+++ client/src/pages/Chat.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Messages from '../components/Messages/Messages';
+
+const Chat = () => {
+
+  return (
+    <div>
+      <p>Chat</p>
+      <Messages />
+    </div>
+  )
+};
+
+export default Chat;
