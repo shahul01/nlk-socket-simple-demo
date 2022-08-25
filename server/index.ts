d7b19e870957f2385c857ed600eb29db183cc8be
Shahul Hammeed <53307156+shahul01@users.noreply.github.com>
2022-08-25 06:22:05 +0530

    fix: added socket codes without any events

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,31 @@
-import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
 import express from 'express';
+import http from 'http';
 import indexRouter from './routes/indexRoutes';
 import { consoleLine } from './helpers/misc';
 
 const app = express();
 const server = http.createServer(app);
+const io  = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    // origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 const port = process.env?.PORT || 8000;
 
-
+app.use(cors());
 app.use(indexRouter);
+
+
+io.on('connection', (socket) => {
+  console.log(`socket.id: `, socket.id);
+
+});
+
+
 
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
